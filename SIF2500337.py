883701a40607009be67a01aaaf258c82857abb98
mhadthettiarachchi-code <mhadthettiarachchi@gmail.com>
2026-04-20 12:13:41 +0530

    Initial food ordering system

---
+++ SIF2500337.py
@@ -0,0 +1,42 @@
+#Food Ordering System
+
+print("MENU")
+
+print("1. Burger - 500")
+
+print("2. Pizza - 1000")
+
+print("3. Juice - 300")
+
+
+choice = input("Enter your choice (burger/pizza/juice): ").lower()
+
+quantity = int(input("Enter quantity: "))
+
+if choice == "burger":
+
+    price = 500
+
+elif choice == "pizza":
+
+    price = 1000
+
+elif choice == "juice":
+
+    price = 300
+
+else:
+
+    print("Wrong Selection Try Again!")
+
+    exit()
+
+
+total = price * quantity
+
+
+print("You ordered:", choice)
+
+print("Quantity:", quantity)
+
+print("Total bill:", total)
